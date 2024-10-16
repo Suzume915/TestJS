8248257b3574505dec42786b5822ec50ba84bcfc
Suzume915 <mesuzu900@gmail.com>
2024-10-16 10:53:32 +0630

    Third commit

--- One.js
+++ One.js
@@ -1,3 +1,6 @@
 const user = {user1:"Eula",user2:"Yae Miko",user3:"Shinobu"}
 const archon = {Electro:"Raiden Shogun",Geo:"Zhongli",Hydro:"Furina",Dendro:"Nahida"}
-console.log(user.user1)
+const db = []
+
+db.push(archon)
+console.log(db)
